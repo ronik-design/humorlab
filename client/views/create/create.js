754096245dc4c55f89e15110796b8e84ffb50c7c
Mark Catalano <catalano.m@gmail.com>
2013-07-28 17:49:19 -0400

    Added Bob audio on the create page

--- client/views/create/create.js
+++ client/views/create/create.js
@@ -29,33 +29,83 @@ Template.create.events({
                 score: 0
             });
 
-            Meteor.Router.to("/caption/" + id);
+
+            var compliment = getRandom(compliments);
+            compliment.bind("ended", function() {
+                Meteor.Router.to("/caption/" + id);
+            });
+
+            compliment.play();
+
+
         }
     },
 
     'click .difficulty': function (e) {
-        console.log($(e.target).val());
+        var difficulty = $(e.target).val();
+
+
+        $('.caption-enter').removeClass('easy, medium, hard').addClass(difficulty);
+
+        if (difficulty == 'easy') {
+
+        } else if(difficulty == 'medium') {
+
+            bob("/audio/category/categoriesare.wav");
+        }
+
+    },
+
+    'click #bob': function(e) {
+        var insightAudio = Session.get('currentCartoon').insightAudio;
+
+        if(insightAudio){
+            bob(insightAudio);
+        }
+
     }
+
 });
+
+function getRandom(arr){
+    return arr[Math.floor(Math.random() * (arr.length - 1))];
+}
+
+function bob(url) {
+    buzz.all().stop();
+
+    var $bob = $("#bob"),
+        sound = new buzz.sound(url);
+
+    sound.bind("ended", function() {
+        $bob.removeClass("speaking");
+    });
+    $bob.addClass("speaking");
+    sound.play();
+}
+
+    // CLOSING
 var closing = new buzz.sound('/audio/closing/closing.wav'),
 
-// COMPLIMENT
+    // COMPLIMENT
     compliment1 = new buzz.sound('/audio/compliment/compliment1.wav'),
     compliment2 = new buzz.sound('/audio/compliment/compliment2.wav'),
 
-
-
-// LAUGH
+    // LAUGH
     laugh1 = new buzz.sound('/audio/laugh/laugh_track1.wav'),
     laugh2 = new buzz.sound('/audio/laugh/laugh_track2.wav'),
     laugh3 = new buzz.sound('/audio/laugh/laugh_track3.wav'),
 
-// ZINGER
+    // ZINGER
     zinger1 = new buzz.sound('/audio/zinger/zinger1.wav'),
     zinger2 = new buzz.sound('/audio/zinger/zinger2.wav'),
     zinger3 = new buzz.sound('/audio/zinger/zinger3.wav');
 
-var closings = new buzz.group([closing]);
-var compliments = new buzz.group([compliment1, compliment2]);
-var laughs = new buzz.group([laugh1, laugh2, laugh3]);
-var zingers = new buzz.group([zinger1, zinger2, zinger3]);
+var closings = [closing],
+    compliments = [compliment1, compliment2],
+    laughs = [laugh1, laugh2, laugh3],
+    zingers = [zinger1, zinger2, zinger3];
+
+
+
+
